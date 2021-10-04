c2d65cfa217748a2133ea1d3b11a39b076ada09b
kyoungkyoung <wkjang4@gmail.com>
2021-10-05 00:09:19 +0900

    성직자 Cleric 작성

--- bin/dart_exam01.dart
+++ bin/dart_exam01.dart
@@ -1,4 +1,38 @@
-void main(List<String> arguments) {
-  print('Hello world');
+class Hero {
+  String name;
+  int hp;
+
+  Hero(this.name, this.hp);
+
+  void attack(){
+    print('공격했다');
+  }
+}
+
+// const int maxHp = 50;
+// const int maxMp = 10;
+
+class Cleric extends Hero{
+  // 상수 필드로 선언
+  static final int maxHp = 50;
+  static final int maxMp = 10;
+
+  int mp = maxMp;
+
+  Cleric(String name, int hp) : super(name, hp);
+
+}
+
+
+
+void main(List<String> arguments){
+  final hong = Hero('홍길동', 30);
+  final lim = Hero('임꺽정', 100);
+
+  print(hong.hashCode);
+  print(lim.hashCode);
+
+  final cleric = Cleric('우서', 300);
+
 }
 
